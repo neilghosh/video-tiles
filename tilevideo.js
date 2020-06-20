c61335f4092b0f1d71da252bd26648dc73e6de08
Sabyasachi Ghosh <sabyasachi.ghosh@salesforce.com>
2020-06-20 19:39:27 +0530

    Added Better Feed List

--- tilevideo.js
+++ tilevideo.js
@@ -1,17 +1,23 @@
 var players = [];
 
-
-
 var eclipseVideos = [
   { key: "rD6IeVB9Ayk", title: "Eithiopia" },
-  { key: "56rpzC0RA04", title: "Eithiopia" },
-  { key: "_tEab2xekhQ", title: "Eithiopia" },
-  { key: "3jYS_XKoJ6g", title: "Eithiopia" },
-  { key: "EEIk7gwjgIM", title: "SomeTitle" }
+  { key: "-DnLP7e3XX0", title: "Sirsa" },
+  { key: "jmnj-k_mt2Q", title: "Hanle" },
+  { key: "hstNxjrNfXw", title: "Kodaikanal" },
+  { key: "eQzDgwpKZFk", title: "Okinawa" },
+  { key: "RyRDDZhIh1c", title: "Irkutsk" },
+  { key: "GXI3_n4prMs", title: "TURKEY" },
+  { key: "56rpzC0RA04", title: "Kurukshetra" },
+  { key: "_tEab2xekhQ", title: "Suratgarh" },
+  { key: "3jYS_XKoJ6g", title: "Taiwan" },
 ];
 
 var defaultVideos = [
-  { key: "EEIk7gwjgIM", title: "SomeTitle" }
+  { key: "EEIk7gwjgIM", title: "NASA ISS Live Stream" },
+  { key: "DDU-rZs-Ic4", title: "Space Official" },
+  { key: "21X5lGlDOfg", title: "NASA TV" },
+  { key: "5_rLJNq7Rw8", title: "Earth From Sace" }
 ];
 
 
